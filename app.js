f245dfd70f43bfd8510cf50247ac49178ae6dcf5
Jasmine Persson <jasmine.persson@hyperisland.se>
2020-10-14 17:17:04 +0200

    Added cors in app.js

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const logger = require('morgan');
-
 const app = express();
+const cors = require("cors");
+
+app.use(require("cors")());
+
+app.use(
+    cors({
+        origin: ["http://localhost:4000"],
+        credentials: true
+    })
+);
 
 app.use(logger('dev'));
 app.use(express.json());
